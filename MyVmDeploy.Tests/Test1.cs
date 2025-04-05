f9c2838adbe0f0c0373f23ff6e04f75835477717
KanwarDanish <gitoutsidecolab@zodiac.tech>
2025-04-05 14:06:47 -0400

    MSTest added

---
+++ MyVmDeploy.Tests/Test1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyVmDeploy.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Addition_TwoPlusTwo_EqualsFour()
+        {
+            // Arrange
+            int a = 2;
+            int b = 2;
+
+            // Act
+            int result = a + b;
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+    }
+}
--- VMDeployment/obj/Debug/net8.0/VMDeployment.AssemblyInfo.cs
+++ VMDeployment/obj/Debug/net8.0/VMDeployment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VMDeployment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66bdbe901b497d48b2a486435771475ad14670c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("VMDeployment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VMDeployment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
